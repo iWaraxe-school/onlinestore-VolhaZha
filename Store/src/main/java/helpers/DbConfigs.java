0c6265b190c2ba5c0bc4e4a0d4089224ac2ced47
Olka7Olka <Olga_-93@mail.ru>
2022-12-29 18:30:23 +0100

    Patterns: Singleton + Builder.

---
+++ Store/src/main/java/helpers/DbConfigs.java
@@ -0,0 +1,9 @@
+package helpers;
+
+public class DbConfigs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "OPQ123!OPQ";
+    protected String dbName = "java1";
+}
